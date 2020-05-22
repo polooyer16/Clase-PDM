b3cb4766bce5b97992c8ec0659f1d88bcba01ef0
leooyer16 <54417432+leooyer16@users.noreply.github.com>
2020-05-22 16:47:05 -0500

    4/46

--- posts_dm/lib/main.dart
+++ posts_dm/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:posts_dm/posts_list.dart';
+
+//Leopoldo Garcia Oyervides
    void main(){
      //Metodo main que manda llmar a PostList
   runApp(MaterialApp(
